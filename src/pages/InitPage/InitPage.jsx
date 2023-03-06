76276735385adbb7a3d50f1c289f1cc9696783c1
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-07 02:25:46 +0900

    Feat: 초기화면 추가

---
+++ src/pages/InitPage/InitPage.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import * as S from './InitPage.style';
+import LottieAni from '../../utils/lottieAni';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import InitPageAni01 from '../../assets/InitPageAni01.json';
+import InitPageAni02 from '../../assets/InitPageAni02.json';
+import InitPageAni03 from '../../assets/InitPageAni03.json';
+import { useNavigate } from 'react-router-dom';
+
+function Hero({ num, title, desc, aniName }) {
+  return (
+    <>
+      <S.HeroNum>{num}</S.HeroNum>
+      <S.HeroTitle>{title}</S.HeroTitle>
+      <S.HeroDesc>{desc}</S.HeroDesc>
+      <S.HeroIMG>
+        <LottieAni aniName={aniName} />
+      </S.HeroIMG>
+    </>
+  );
+}
+
+function InitPage() {
+  const navigate = useNavigate();
+  const NavigateToSignUp = () => {
+    navigate('/login');
+  };
+
+  return (
+    <S.Container>
+      <S.MainWrapper>
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{
+            clickable: true,
+            // bulletClass: CustomBullet,
+          }}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <Hero
+              num="01"
+              title="바나프레소 앱에서
+              기다림 없이
+              바로 주문하세요"
+              desc="커피가 필요한 지금,
+              때와 장소에 상관 없이 필요할 때 바로 주문"
+              aniName={InitPageAni01}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Hero
+              num="02"
+              title="
+              카운터 근처에
+              있지 않아도 괜찮아요.
+              "
+              desc="커피를 기다리고 계신가요?
+              카운터 근처에 있지 않아도 괜찮아요.
+              메뉴가 나오면 알려드립니다."
+              aniName={InitPageAni02}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Hero
+              num="03"
+              title="
+              바나프레소의
+              새로운 소식과 혜택이
+              모두 이곳에."
+              desc="반갑고 새로운 소식과 혜택이
+              모두 이곳에 모여있습니다."
+              aniName={InitPageAni03}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </S.MainWrapper>
+      <S.Button onClick={NavigateToSignUp}>회원가입 / 로그인</S.Button>
+    </S.Container>
+  );
+}
+
+export default InitPage;
---
+++ src/pages/InitPage/InitPage.style.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  border: 1px solid;
+`;
+
+export const MainWrapper = styled.div`
+  margin-top: 100px;
+  height: 600px;
+  padding: 0 20px;
+  border: 1px solid;
+`;
+export const HeroNum = styled.div`
+  width: 30px;
+  color: var(--main-color);
+  font-size: 26px;
+  font-weight: bold;
+  border-bottom: 3px solid;
+`;
+export const HeroTitle = styled.h1`
+  margin-top: 20px;
+  font-size: 26px;
+  line-height: 40px;
+`;
+export const HeroDesc = styled.p`
+  margin-top: 16px;
+  line-height: 26px;
+  color: #d7a1b5;
+`;
+export const HeroIMG = styled.div`
+  height: 300px;
+  overflow: hidden;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 50px;
+  margin-top: 180px;
+  color: #fff;
+  background-color: var(--main-color);
+`;
