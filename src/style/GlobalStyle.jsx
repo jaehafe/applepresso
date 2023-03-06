aad40fbae034a0bedc6927c46585228aa89fe7ee
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-07 02:24:51 +0900

    Feat: Global 스타일 설정

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -1,9 +1,20 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`  
+export const GlobalStyles = createGlobalStyle`
+  body {
+    width: 500px;
+    margin: auto;
+  }
+
+  #root {
+    width: 100%;
+    min-height: 100vh;
+  }
+
+
   * {
   font-family: "Pretendard", -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-  --main-color: #f48225;
+  --main-color: #F965A3;
   --box-shadow: 0 0 0 1px rgb(0 0 0 / 7%), 0 2px 4px rgb(0 0 0 / 5%),
     0 12px 24px rgb(0 0 0 / 5%);
   }
@@ -58,4 +69,6 @@ export const GlobalStyles = createGlobalStyle`
   iframe {
     border: 0;
   }
+
+  
 `;
