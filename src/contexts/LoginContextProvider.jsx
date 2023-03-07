7f75841f2a2a15f095f115651dcca9b8d6b9e1fd
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-08 00:51:42 +0900

    Feat: Login Context API 전역 설정

---
+++ src/contexts/LoginContextProvider.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { axiosInstance } from '../constants/axios';
+import { requestLogin } from '../constants/request';
+
+export const LoginContext = createContext();
+
+// localStorage.setItem('banapressoToken', JSON.stringify(res.data.accessToken));
+function LoginContextProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem('BANA_USER')) || null
+  );
+  console.log('currentUser', currentUser);
+
+  const login = async (info) => {
+    const data = await requestLogin(info);
+    setCurrentUser(data);
+    return data;
+  };
+
+  useEffect(() => {
+    localStorage.setItem('BANA_USER', JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <LoginContext.Provider value={{ login, currentUser }}>
+      {children}
+    </LoginContext.Provider>
+  );
+}
+
+export default LoginContextProvider;
