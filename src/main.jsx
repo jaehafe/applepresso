fbed021e4f6ea94e8b18e32058d4f94806371cb7
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-08 00:54:00 +0900

    Feat: LoginContextProvider -> main.jsx 전역 선언

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ResetCss from './style/ResetCss';
 import { GlobalStyles } from './style/GlobalStyle';
+import LoginContextProvider from './contexts/LoginContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
+    <LoginContextProvider>
       <ResetCss />
       <GlobalStyles />
       <App />
-    </>
+    </LoginContextProvider>
   </React.StrictMode>
 );
