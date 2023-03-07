4d5c5fe9d549fbe6257551e31e30f2431b583a7f
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-07 22:16:34 +0900

    Feat: 회원가입 페이지, 홈 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import InitPage from './pages/InitPage/InitPage';
 import Login from './pages/Login/Login';
+import Signup from './pages/Singup/Signup';
 
 const router = createBrowserRouter([
   { path: '/', element: <InitPage /> },
   { path: '/login', element: <Login /> },
-  // { path: '/signup', element: <SignUp /> },
+  { path: '/signup', element: <Signup /> },
+  { path: '/home', element: <Home /> },
 ]);
 
 function App() {
