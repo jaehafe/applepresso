302bb7f9d024f4415334334bf1dd1d29b89b7ddb
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-08 03:23:29 +0900

    Feat: main 페이지 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import InitPage from './pages/InitPage/InitPage';
 import Login from './pages/Login/Login';
 import Signup from './pages/Singup/Signup';
+import MainNav from './components/MainNav/MainNav';
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+      <MainNav />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   { path: '/', element: <InitPage /> },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <Signup /> },
-  { path: '/home', element: <Home /> },
+  {
+    path: '/main',
+    element: <Layout />,
+    children: [{ path: '/main/home', element: <Home /> }],
+  },
 ]);
 
 function App() {
