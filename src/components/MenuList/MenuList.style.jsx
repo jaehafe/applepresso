40047ec6fdd2f5b3231aa165c57210f9841990fd
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-08 23:31:57 +0900

    Feat: 주문 페이지 MenuList 컴포넌트화

---
+++ src/components/MenuList/MenuList.style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border: 1px solid;
+  margin: 170px 0 100px;
+  min-height: 100vh;
+`;
+
+export const MenuLists = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 0;
+`;
+
+export const MenuList = styled.li`
+  border: 1px solid;
+`;
