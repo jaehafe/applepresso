b10189c509ce997a347d507bddd98fa76c045ef3
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-09 16:44:00 +0900

    Feat: 메뉴 상세페이지 퍼블리싱

--- src/pages/Order/MenuDetail/MenuDetail.jsx
+++ src/pages/Order/MenuDetail/MenuDetail.jsx
@@ -1,25 +1,77 @@
 import React from 'react';
 import * as S from './MenuDetail.style';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { menuDatas } from '../../../constants/data/menuDatas';
 
 function MenuDetail() {
+  const navigate = useNavigate();
   const { id } = useParams();
   console.log('id', id);
   const menuData = menuDatas.find((menu) => menu.id === id);
   console.log('menuData', menuData);
+  const {
+    title,
+    ENTitle,
+    desc,
+    discountRate,
+    isBest,
+    isNew,
+    isSoldOut,
+    price,
+    tags,
+    thumbnail,
+  } = menuData;
+
+  const handleNavigate = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <S.HeaderContainer>
         <S.HeaderWrapper>
           <S.HeaderTitleWrapper>
-            <S.StyledBsArrowLeft />
+            <S.StyledBsArrowLeft onClick={handleNavigate} />
             <S.HeaderTitle>{menuData.title}</S.HeaderTitle>
           </S.HeaderTitleWrapper>
-          <S.StyledAiOutlineUnorderedList />
+          <S.StyledAiOutlineUnorderedList onClick={handleNavigate} />
         </S.HeaderWrapper>
       </S.HeaderContainer>
-      <S.MenuContainer>123</S.MenuContainer>
+      <S.MenuContainer>
+        <S.IMGWrapper>
+          <S.MenuIMG src={thumbnail} />
+        </S.IMGWrapper>
+        <S.MenuInfo>
+          <S.MenuTitle>{title}</S.MenuTitle>
+          <S.ENTitle>{ENTitle}</S.ENTitle>
+          <S.Desc>{desc}</S.Desc>
+        </S.MenuInfo>
+        <S.MenuDetailInfo>
+          <S.Price>{price.toLocaleString()}원</S.Price>
+          <S.OptionWrapper>
+            <S.Option>HOT</S.Option>
+            <S.Option>ICE</S.Option>
+          </S.OptionWrapper>
+        </S.MenuDetailInfo>
+      </S.MenuContainer>
+      <S.Divider />
+      <S.OrderOptionContainer>
+        <S.MenuSelectWrapper>
+          <S.CountSpan>수량</S.CountSpan>
+          <S.CountControlWrapper>
+            <S.CountMinus>-</S.CountMinus>
+            <S.CountQty>1</S.CountQty>
+            <S.CountPlus>+</S.CountPlus>
+          </S.CountControlWrapper>
+        </S.MenuSelectWrapper>
+        <S.MenuSelectWrapper>
+          <S.CountSpan>옵션</S.CountSpan>
+        </S.MenuSelectWrapper>
+      </S.OrderOptionContainer>
+      <S.OrderButtons>
+        <S.AddCartButton>담기</S.AddCartButton>
+        <S.OrderNowButton>바로 주문하기</S.OrderNowButton>
+      </S.OrderButtons>
     </>
   );
 }
--- src/pages/Order/MenuDetail/MenuDetail.style.jsx
+++ src/pages/Order/MenuDetail/MenuDetail.style.jsx
@@ -28,7 +28,7 @@ export const HeaderContainer = styled.div`
   height: 80px;
   background-color: #fff;
   padding: 0 20px;
-  border-bottom: 1px solid var(--main-color);
+  border-bottom: 1px solid rgb(239, 224, 224);
 `;
 
 export const HeaderWrapper = styled.div`
@@ -50,5 +50,155 @@ export const MenuContainer = styled.div`
   margin-top: 100px;
   border: 1px solid;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+export const IMGWrapper = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+export const MenuIMG = styled.img`
+  object-fit: contain;
+  background-color: rgb(239, 224, 224);
+  border-radius: 40px;
+  object-fit: contain;
+`;
+
+export const MenuInfo = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid rgb(239, 224, 224);
+`;
+export const MenuTitle = styled.h3`
+  font-size: 30px;
+`;
+export const ENTitle = styled.p`
+  font-size: 20px;
+  color: rgb(174, 153, 153);
+`;
+export const Desc = styled.p`
+  font-size: 23px;
+  text-align: center;
+  line-height: 30px;
+  color: grey;
+  font-weight: 350;
+`;
+export const MenuDetailInfo = styled.div`
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  font-size: 26px;
+`;
+export const Price = styled.div`
+  color: rgb(171, 134, 134);
+`;
+export const OptionWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+`;
+export const Option = styled.button`
+  border: 2px solid rgb(213, 197, 197);
+  padding: 8px 20px;
+  border-radius: 20px;
+  color: rgb(213, 197, 197);
+  background-color: rgb(245, 238, 238);
+  font-size: 20px;
+
+  &:hover {
+    background-color: #fff;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+// divider
+export const Divider = styled.div`
+  border-top: 1px solid rgb(213, 197, 197);
+  border-bottom: 1px solid rgb(213, 197, 197);
+  height: 10px;
+  background-color: rgb(245, 238, 238);
+`;
+
+// 주문 옵션
+export const OrderOptionContainer = styled.div`
+  border: 1px solid;
+  padding: 0 20px;
+`;
+
+export const MenuSelectWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 25px 0;
+  border-bottom: 1px solid rgb(239, 224, 224);
+`;
+export const CountSpan = styled.span`
+  font-size: 26px;
+`;
+export const CountControlWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const QtyBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border: 2px solid rgb(213, 197, 197);
+  border-radius: 20px;
+  color: rgb(213, 197, 197);
+  font-size: 30px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(245, 238, 238);
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+export const CountMinus = styled(QtyBtn)``;
+export const CountQty = styled.div`
+  font-size: 26px;
+`;
+export const CountPlus = styled(QtyBtn)``;
+
+export const OrderButtons = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+export const OrderButton = styled.button`
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: 26px 0;
+  color: #fff;
+  font-size: 23px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: rgba(211, 86, 107, 0.8); // hover 시 약간 밝게
+  }
+`;
+export const AddCartButton = styled(OrderButton)`
+  width: 50%;
+  background-color: #d3566b;
+`;
+export const OrderNowButton = styled(OrderButton)`
+  width: 50%;
+  background-color: var(--main-color);
 `;
