315e85c7f32b7a4a9a6bb4deab8f96230c74589d
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-13 17:24:24 +0900

    Feat: axiosFirebase로 useGetMenu 커스텀 훅 추가

---
+++ src/hooks/useGetMenu.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { axiosFirebase } from '../constants/axios';
+
+function useGetMenu(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMenu = async () => {
+    try {
+      setLoading(true);
+      const res = await axiosFirebase.get('/menu.json');
+      const menuArr = Object.values(res.data);
+      setData(menuArr);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useGetMenu;
