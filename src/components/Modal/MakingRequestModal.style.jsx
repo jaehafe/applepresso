ceed9c0da4aa6e7b5ff5757cfb0d6b7e1f11ef3f
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-17 16:24:00 +0900

    Remove: 불필요한 주석 삭제

--- src/components/Modal/MakingRequestModal.style.jsx
+++ src/components/Modal/MakingRequestModal.style.jsx
@@ -13,20 +13,6 @@ export const ModalContainer = styled.div`
   height: 300px;
   border-radius: 20px;
   background-color: #fff;
-  /* display: flex;
-  justify-content: center;
-  align-items: center; */
-
-  //
-  /* position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 10; */
 `;
 
 export const ModalWrapper = styled.div`
