c0e6ce95c6b1aea47f007e3159b0da5b32710866
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-17 19:46:12 +0900

    Feat: header nav, hero nav 레이아웃 완료

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,6 +1,41 @@
 import React, { useContext } from 'react';
 import * as S from './MyPage.style';
 import { LoginContext } from '../../contexts/LoginContextProvider';
+import { Link } from 'react-router-dom';
+
+const HeaderNav = [
+  { title: '카드 관리', navigate: '/main/home', icons: <S.StyledBsCreditCard /> },
+  { title: '설정', navigate: '/main/home', icons: <S.StyledRiListSettingsLine /> },
+  { title: '알림', navigate: '/main/home', icons: <S.StyledBiBell /> },
+  { title: '1:1 문의', navigate: '/main/home', icons: <S.StyledBsChatDots /> },
+];
+
+const HeroNav = [
+  {
+    title: '간편주문',
+    navigate: '/main/home',
+    icons: <S.StyledFiBook />,
+    directionIcon: <S.StyledIoIosArrowForward />,
+  },
+  {
+    title: '주문내역',
+    navigate: '/main/home',
+    icons: <S.StyledIoReceiptOutline />,
+    directionIcon: <S.StyledIoIosArrowForward />,
+  },
+  {
+    title: '약관 및 정책',
+    navigate: '/main/home',
+    icons: <S.StyledBsShieldCheck />,
+    directionIcon: <S.StyledIoIosArrowDown />,
+  },
+  {
+    title: '매장 정보',
+    navigate: '/main/home',
+    icons: <S.StyledBiStoreAlt />,
+    directionIcon: <S.StyledIoIosArrowForward />,
+  },
+];
 
 function MyPage() {
   const { currentUser } = useContext(LoginContext);
@@ -23,8 +58,47 @@ function MyPage() {
         </S.HeaderLeft>
         <S.StyledFiLogOut onClick={handleLogout} />
       </S.HeaderWrapper>
+      <S.HeaderNav>
+        {HeaderNav.map((items) => {
+          const { title, navigate, icons } = items;
+          return (
+            <Link to={navigate} key={title}>
+              <S.HeaderList>
+                {icons}
+                <S.HeaderListTitle>{title}</S.HeaderListTitle>
+              </S.HeaderList>
+            </Link>
+          );
+        })}
+      </S.HeaderNav>
+      <S.HeroNav>
+        {HeroNav.map((items) => {
+          const { title, navigate, icons, directionIcon } = items;
+          return (
+            <Link key={title} to={navigate}>
+              <S.HeroWrapper>
+                <S.HeroTitleWrapper>
+                  {icons}
+                  <S.HeroTitle>{title}</S.HeroTitle>
+                </S.HeroTitleWrapper>
+                {directionIcon}
+              </S.HeroWrapper>
+            </Link>
+          );
+        })}
+      </S.HeroNav>
+      <S.CardContainer>123</S.CardContainer>
+      <S.CardContainer>123</S.CardContainer>
+      <S.CardContainer>123</S.CardContainer>
     </S.Container>
   );
 }
 
 export default MyPage;
+
+{
+  /* <S.HeaderList>
+          <S.StyledBsCreditCard />
+          <S.HeaderListTitle>카드 관리</S.HeaderListTitle>
+        </S.HeaderList> */
+}
