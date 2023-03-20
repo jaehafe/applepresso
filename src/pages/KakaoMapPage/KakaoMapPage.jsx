be48efc9223a963bc83f7b0ab109afe4bde41118
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-21 02:37:45 +0900

    Feat: 경도, 위도 값을 가져와 지도에 마커 표시

--- src/pages/KakaoMapPage/KakaoMapPage.jsx
+++ src/pages/KakaoMapPage/KakaoMapPage.jsx
@@ -4,20 +4,36 @@ import * as S from './KakaoMapPage.style';
 
 const { kakao } = window;
 
-function KakaoMapPage() {
+function KakaoMapPage({ Lat = 37.5900707, Lng = 127.0553692 }) {
   const navigate = useNavigate();
   const handleToBack = () => {
     navigate(-1);
   };
 
   useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
+    mapScript();
+  }, [Lat, Lng]);
+
+  const mapScript = () => {
+    let container = document.getElementById('map');
+    let options = {
+      center: new kakao.maps.LatLng(Lat, Lng),
+      level: 5,
     };
+    //map
     const map = new kakao.maps.Map(container, options);
-  }, []);
+
+    //마커가 표시 될 위치
+    let markerPosition = new kakao.maps.LatLng(Lat, Lng);
+
+    // 마커를 생성
+    let marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 마커를 지도 위에 표시
+    marker.setMap(map);
+  };
 
   return (
     <S.Container>
