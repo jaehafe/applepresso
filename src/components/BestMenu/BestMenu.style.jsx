5d90f598ee6630bea4dd0d37167c6f35f4e9fcc8
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-22 02:07:25 +0900

    Style: 에러 발생시, 다시 시도하기 버튼 스타일링

--- src/components/BestMenu/BestMenu.style.jsx
+++ src/components/BestMenu/BestMenu.style.jsx
@@ -100,5 +100,24 @@ export const MenuTitle = styled.div`
 `;
 export const MenuCount = styled.div``;
 
-export const RetryButton = styled.div``;
+export const ButtonContainer = styled.div`
+  border: 1px solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 60px;
+  border-radius: 16px;
+  background-color: #fff;
+`;
+export const RetryButton = styled.button`
+  border: 1px solid;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: var(--button-color);
+  color: #fff;
+`;
+export const TitleWrapper = styled.div``;
+export const RetryTitle = styled.div``;
+export const RetrySubtitle = styled.div``;
 // export const Container = styled.div``
