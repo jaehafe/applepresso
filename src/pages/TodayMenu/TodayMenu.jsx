daa29712ff729e063c3d8163b431a7751c8ce908
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-22 21:41:18 +0900

    Feat: 오늘의 메뉴 페이지
    - error boundary: 에러 발생 시, 비동기 부분만 다시시도 할 수 있게 버튼 추가
    - 오늘부터 7일 이후의 날짜를 배열로 지정해 랜덤 메뉴 추천 기능 추가

--- src/pages/TodayMenu/TodayMenu.jsx
+++ src/pages/TodayMenu/TodayMenu.jsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SelectStoreNav from '../../components/SelectStoreNav/SelectStoreNav';
 import * as S from './TodayMenu.style';
+import banner from '../../assets/homeBanner/banner1.jpeg';
+import useGetMenu from '../../hooks/useGetMenu';
+import { formatPrice } from '../../utils/format';
+import Loading from '../../components/Loading.jsx/Loading';
+
+const getDayString = (day) => {
+  const dayNames = ['일', '월', '화', '수', '목', '금', '토'];
+  return dayNames[day];
+};
+
+const getFormattedDate = (date) => {
+  return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+};
 
 function TodayMenu() {
   const navigate = useNavigate();
+  const [dates, setDates] = useState([]);
+  const { data, loading, error, refetchData } = useGetMenu('/menu');
+
   const handleToBack = () => {
     navigate(-1);
   };
+
+  useEffect(() => {
+    const newDates = [];
+    for (let i = 0; i < 7; i++) {
+      const currentDate = new Date();
+      // setDate 월, 일을 나타내는 정수
+      currentDate.setDate(currentDate.getDate() + i);
+      newDates.push(currentDate);
+    }
+    setDates(newDates);
+  }, []);
+
+  const shuffleArray = (array) => {
+    const newArray = array.slice();
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  const getRandomItems = (array, count) => {
+    const shuffledArray = shuffleArray(array);
+    return shuffledArray.slice(0, count);
+  };
+
+  const HandleNavigateToDetail = (id) => {
+    navigate(`/menuDetail/${id}`);
+  };
+
+  // error boundary
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <S.Container>
@@ -17,7 +68,70 @@ function TodayMenu() {
             <S.HeaderTitle>오늘의 메뉴</S.HeaderTitle>
           </S.HeaderTitleWrapper>
         </S.HeaderWrapper>
+        <SelectStoreNav top="80px" />
+        <S.HeroAlertContainer>
+          <S.HeroTitleWrapper>
+            <S.HeroTitle>오늘의 메뉴</S.HeroTitle>
+            <S.HeroWarningWrapper>
+              <S.HeroWarning>배달 시 미적용</S.HeroWarning>
+              <S.HeroWarning>일부매장 제외</S.HeroWarning>
+            </S.HeroWarningWrapper>
+          </S.HeroTitleWrapper>
+          <S.HeroParagraphWrapper>
+            <S.HeroParagraph>매일매일 달라지는 메뉴, 기분 좋은 가격!</S.HeroParagraph>
+            <S.HeroParagraph>놓치지 마세요!</S.HeroParagraph>
+          </S.HeroParagraphWrapper>
+        </S.HeroAlertContainer>
       </S.HeaderContainer>
+      <S.Main>
+        {error && (
+          <S.ButtonContainer>
+            <S.TitleWrapper>
+              <S.RetryTitle>요청사항을 처리하는데 실패했습니다.</S.RetryTitle>
+              <S.RetrySubtitle>잠시 후 다시 시도해주세요</S.RetrySubtitle>
+            </S.TitleWrapper>
+            <S.RetryButton
+              onClick={() => {
+                refetchData();
+              }}
+            >
+              다시 시도하기
+            </S.RetryButton>
+          </S.ButtonContainer>
+        )}
+        {getRandomItems(data, 7).map((item, index) => {
+          const { id, thumbnail, title, ENTitle, discountRate, price } = item;
+          const discountPrice = price - price * (1 - discountRate / 100);
+          const discountedPrice = price - discountPrice;
+          const date = dates[index];
+          return (
+            <S.MainRow key={id}>
+              <S.MainRowAside>
+                <S.Date>{date && getFormattedDate(date)}</S.Date>
+                <S.DayOfWeek>{date && getDayString(date.getDay())}</S.DayOfWeek>
+              </S.MainRowAside>
+              <S.MainBorder>
+                <S.StyledAiOutlineRightCircle />
+              </S.MainBorder>
+              <S.MainRowHero onClick={() => HandleNavigateToDetail(id)}>
+                <S.MenuWrapper>
+                  <S.Thumbnail src={thumbnail} alt={title} />
+                  <S.MenuInfo>
+                    <S.MenuTitle>{title}</S.MenuTitle>
+                    <S.MenuEngTitle>{ENTitle}</S.MenuEngTitle>
+                    <S.MenuPriceWrapper>
+                      <S.OriginalPrice>{formatPrice(price)}원</S.OriginalPrice>
+                      <S.DiscountedPrice>
+                        {formatPrice(discountedPrice)}원
+                      </S.DiscountedPrice>
+                    </S.MenuPriceWrapper>
+                  </S.MenuInfo>
+                </S.MenuWrapper>
+              </S.MainRowHero>
+            </S.MainRow>
+          );
+        })}
+      </S.Main>
     </S.Container>
   );
 }
--- src/pages/TodayMenu/TodayMenu.style.jsx
+++ src/pages/TodayMenu/TodayMenu.style.jsx
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
-import { BsArrowLeft } from 'react-icons/all';
+import { BsArrowLeft, AiOutlineRightCircle } from 'react-icons/all';
 
+export const StyledAiOutlineRightCircle = styled(AiOutlineRightCircle)``;
 export const StyledBsArrowLeft = styled(BsArrowLeft)`
   font-size: 24px;
   color: #333;
   cursor: pointer;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  /* background-color: var(--background-color); */
+  min-height: 100vh;
+`;
 
 export const HeaderContainer = styled.div`
-  z-index: 10;
+  z-index: 3;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -40,3 +44,160 @@ export const HeaderTitleWrapper = styled.div`
 export const HeaderTitle = styled.h2`
   font-weight: 500;
 `;
+
+export const HeroAlertContainer = styled.div`
+  z-index: 2;
+  position: fixed;
+  top: 130px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 500px;
+  height: auto;
+  padding: 50px 20px;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid var(--division);
+  background-color: #7171dd;
+  color: #fff;
+`;
+export const HeroTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const HeroTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 400;
+`;
+export const HeroWarningWrapper = styled.div`
+  margin-left: 10px;
+  display: flex;
+  gap: 6px;
+`;
+export const HeroWarning = styled.span`
+  font-size: 14px;
+  border: 1px solid var(--division);
+  border-radius: 16px;
+  padding: 6px 10px;
+`;
+export const HeroParagraphWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+export const HeroParagraph = styled.p``;
+
+// body
+
+export const Main = styled.div`
+  padding: 360px 20px 10px;
+  min-height: 100vh;
+  background-color: var(--background-color);
+  display: grid;
+  grid-template-rows: repeat(7, 1fr);
+`;
+export const MainRow = styled.div`
+  height: 140px;
+
+  display: grid;
+  grid-template-columns: 1fr 0.3fr 5fr;
+`;
+
+export const MainRowAside = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 6px;
+
+  color: grey;
+  font-weight: 600;
+`;
+export const Date = styled.div``;
+export const DayOfWeek = styled.div`
+  text-align: end;
+`;
+
+//
+export const MainBorder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+//
+export const MainRowHero = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const MenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding: 10px 20px;
+  width: 90%;
+  height: 90%;
+  background-color: #fff;
+  border-radius: 20px;
+`;
+export const Thumbnail = styled.img`
+  height: 80px;
+  width: 80px;
+  object-fit: cover;
+  margin-right: 20px;
+`;
+export const MenuInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+`;
+export const MenuTitle = styled.h3`
+  font-size: 22px;
+`;
+export const MenuEngTitle = styled.h4`
+  font-size: 14px;
+  font-weight: 400;
+  color: var(--subtitle-color);
+`;
+
+export const MenuPriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+export const OriginalPrice = styled.div`
+  color: var(--division);
+  text-decoration: line-through;
+`;
+export const DiscountedPrice = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  color: var(--main-color);
+`;
+// export const Container = styled.div``
+
+// error boundary
+export const ButtonContainer = styled.div`
+  border: 1px solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 60px;
+  border-radius: 16px;
+  background-color: #fff;
+`;
+export const RetryButton = styled.button`
+  border: 1px solid;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: var(--button-color);
+  color: #fff;
+`;
+export const TitleWrapper = styled.div``;
+export const RetryTitle = styled.div``;
+export const RetrySubtitle = styled.div``;
